f5241f21aec466a9dca559e5debbf895be213067
ee7klt <kltah1@gmail.com>
2013-09-18 12:44:01 +0800

    Wired collection into postLists template client/view/posts/posts_list.js

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,24 +1,9 @@
 // postLists manager
 
 
-var postsData = [
-	{
-		title: 'Hello world',
-		author: 'Sacha Greif',
-		url: 'http://sachagreif.com/introducing-telescope/'
-	},
-	{
-		title: 'Hello world',
-		author: 'Tom Coleman',
-		url: 'http://meteor.com'
-	},
-	{
-		title: 'Hello world',
-		author: 'Tom Coleman',
-		url: 'http://themeteorbook.com'
-	}
-];
 
 Template.postsList.helpers({
-	posts: postsData
+	posts: function() {
+		return Posts.find();
+	}
 });
